f780016ba2346c46a1511753b800b194904d909e
h80h <h80h525@gmail.com>
2025-03-02 04:14:03 +0800

    Redesign how sketch pad made to recreate pad eazy

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
+let allContainer = document.querySelector(".allContainer");
 let container = document.querySelector(".container");
 let grid = document.querySelector(".grid");
-for(i = 0; i < 15; i++){
-  let newGrid = grid.cloneNode(true);
-  container.append(newGrid)
-}
+let k;
 
-let allContainer = document.querySelector(".allContainer");
-for(i = 0; i < 15; i++){
-  let newContainer = container.cloneNode(true);
-  allContainer.append(newContainer);
-}
+function createPad(j) {
+  for(i = 0; i < j; i++){
+    let newGrid = grid.cloneNode(true);
+    container.append(newGrid)
+  }
 
+  for(i = 0; i < j; i++){
+    let newContainer = container.cloneNode(true);
+    allContainer.append(newContainer);
+  }
+}
+createPad(15);
 
 allContainer.addEventListener("mouseover", (e) => {
   console.log(e.target);
   e.target.setAttribute("style", "background-color: #333333");
-})
+});
+
+let newPad = document.querySelector(".newPad");
+newPad.addEventListener("click", () => {
+  k = prompt("how many squares per line?\n\(no larger than 100\)")
+  allContainer.replaceChildren();
+  container.replaceChildren();
+  
+  createPad(k);
+});
+
+
