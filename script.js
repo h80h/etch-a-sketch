0ed4416e4da32eb8783c29060e843f762904bcfc
h80h <h80h525@gmail.com>
2025-03-02 22:47:08 +0800

    Optimize codes

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
 let allContainer = document.querySelector(".allContainer");
 let container = document.querySelector(".container");
 let grid = document.querySelector(".grid");
-let k;
 
 function createPad(j) {
   for(i = 0; i < j; i++){
     grid.setAttribute("data-count", "0");
-    let newGrid = grid.cloneNode(true);
+    let newGrid = grid.cloneNode();
     container.append(newGrid)
   }
 
   for(i = 0; i < j; i++){
     let newContainer = container.cloneNode(true);
     allContainer.append(newContainer);
-  }
-}
-
-function getAlpha(rgbaString) {
-  // Check if the string is in rgba format
-  if (rgbaString.includes('rgba')) {
-      // Extract the alpha channel value
-      let alpha = rgbaString.match(/rgba\(\s*(\d+),\s*(\d+),\s*(\d+),\s*([\d\.]+)\s*\)/)[4];
-      
-      return parseFloat(alpha);
-  } else {
-      return null; // or handle it as needed
   }
 }
 
@@ -37,13 +24,12 @@ allContainer.addEventListener("mouseover", (e) => {
   
   e.target.setAttribute("data-count", `${thisCount}`);
   
-  let lastAlpha = getAlpha(e.target.style.backgroundColor);
   let thisAlpha;
 
   if(Number(thisCount) >= 11){
     thisAlpha = 1.0;
   } else {
-    thisAlpha = (lastAlpha + 0.1).toFixed(1);
+    thisAlpha = (thisCount * 0.1).toFixed(1);
   }
 
   console.log(`thisAlpha: ${thisAlpha}`)
@@ -52,11 +38,9 @@ allContainer.addEventListener("mouseover", (e) => {
   e.target.setAttribute("style", `background-color: ${newRGBAString}`);
 });
 
-
-
 let newPad = document.querySelector(".newPad");
 newPad.addEventListener("click", () => {
-  k = prompt("how many squares per line?\n\(no larger than 100\)")
+  let k = prompt("how many squares per line?\n\(no larger than 100\)")
   allContainer.replaceChildren();
   container.replaceChildren();
   
