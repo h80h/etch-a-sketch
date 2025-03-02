87edf1bd582d3bfa33935fe3662346c8ba6febf8
h80h <h80h525@gmail.com>
2025-03-02 17:00:25 +0800

    Implement progressive darkening effect

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let k;
 
 function createPad(j) {
   for(i = 0; i < j; i++){
+    grid.setAttribute("data-count", "0");
     let newGrid = grid.cloneNode(true);
     container.append(newGrid)
   }
@@ -14,12 +15,44 @@ function createPad(j) {
     allContainer.append(newContainer);
   }
 }
+
+function getAlpha(rgbaString) {
+  // Check if the string is in rgba format
+  if (rgbaString.includes('rgba')) {
+      // Extract the alpha channel value
+      let alpha = rgbaString.match(/rgba\(\s*(\d+),\s*(\d+),\s*(\d+),\s*([\d\.]+)\s*\)/)[4];
+      
+      return parseFloat(alpha);
+  } else {
+      return null; // or handle it as needed
+  }
+}
+
 createPad(15);
 
 allContainer.addEventListener("mouseover", (e) => {
-  console.log(e.target);
-  e.target.setAttribute("style", "background-color: #333333");
+  let lastCount = e.target.getAttribute("data-count");
+  let thisCount = Number(lastCount) +1;
+  console.log(`thisCount: ${thisCount}`);
+  
+  e.target.setAttribute("data-count", `${thisCount}`);
+  
+  let lastAlpha = getAlpha(e.target.style.backgroundColor);
+  let thisAlpha;
+
+  if(Number(thisCount) >= 11){
+    thisAlpha = 1.0;
+  } else {
+    thisAlpha = (lastAlpha + 0.1).toFixed(1);
+  }
+
+  console.log(`thisAlpha: ${thisAlpha}`)
+
+  let newRGBAString = `rgba(51, 51, 51, ${thisAlpha})`
+  e.target.setAttribute("style", `background-color: ${newRGBAString}`);
 });
+
+
 
 let newPad = document.querySelector(".newPad");
 newPad.addEventListener("click", () => {
